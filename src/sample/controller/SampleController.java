47240bd8f503d0b91421ec8e822457c6a03c6bb8
Sheradon Segota <31513874+sseg9117@users.noreply.github.com>
2017-09-07 08:11:16 -0600

    Added controller and runner clases including the main method.

---
+++ src/sample/controller/SampleController.java
@@ -0,0 +1,11 @@
+package sample.controller;
+
+public class SampleController
+{
+	
+	public void start()
+	{
+		System.out.println("This is a sample project to show the github and");
+		System.out.println("eclipse process so I develop muscle memory :D");
+	}
+}
--- src/sample/controller/SampleRunnr.java
+++ src/sample/controller/SampleRunnr.java
@@ -2,9 +2,10 @@ package sample.controller;
 
 public class SampleRunnr
 {
-	public static void main(String[] args)
+	public static void main(String [] args)
 	{
-	
+		SampleController myApp = new SampleController();
+		myApp.start();
 	}
 }
 
