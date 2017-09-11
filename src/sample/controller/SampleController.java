7bd1ef96247f9fdace76b9724c7b7316bc3a24ed
Sheradon Segota <31513874+sseg9117@users.noreply.github.com>
2017-09-11 08:23:42 -0600

    Added my PlayDohCircle class.

--- src/sample/controller/SampleController.java
+++ src/sample/controller/SampleController.java
@@ -1,11 +1,26 @@
 package sample.controller;
 
+import java.util.Scanner;
+
 public class SampleController
 {
-	
+
 	public void start()
 	{
 		System.out.println("This is a sample project to show the github and");
 		System.out.println("eclipse process so I develop muscle memory :D");
+		
+		testScanner();
+	}
+	
+
+	private void testScanner()
+	{
+		Scanner firstScanner; //Decalers a Scanner object.
+		firstScanner = new Scanner(System.in); //Instanciates a Scanner object.
+		System.out.println("Please type your favorite food");
+		String answer = firstScanner.nextLine();
+		System.out.println("Oh, you like to eat " + answer);
+		
 	}
 }
